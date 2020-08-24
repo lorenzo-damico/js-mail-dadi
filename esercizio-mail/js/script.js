3a3dc1b77469ad263a10e9adaf39e75a5e5c1544
lorenzo-damico <loredam@hotmail.it>
2020-08-24 16:52:05 +0200

    esercizio mail completo. aggiunta variabile booleana mail trovata

--- esercizio-mail/js/script.js
+++ esercizio-mail/js/script.js
@@ -1,18 +1,28 @@
 // 1. Definisco una lista di accesso con un array
-var lista = ["indirizzo1@gmail.com", "indirizzo2@gmail.com", "indirizzo3@gmail.com"];
-console.log(lista);
+var emails = ["indirizzo1@gmail.com", "indirizzo2@gmail.com", "indirizzo3@gmail.com"];
+console.log(emails);
 
 // 2. Chiedo all'utente di inserire il suo indirizzo e-mail
-var mailUtente = prompt("Inserisci il tuo indirizzo e-mail");
-console.log(mailUtente);
+var emailUtente = prompt("Inserisci il tuo indirizzo e-mail");
+console.log(emailUtente);
 
-// 3. Controllo che l'indirizzo dell'utente sia nella lista di accesso
-for (var i = 0; i < lista.length; i++) {
+/* 3. Definisco la variabile mailTrovata con valore falso per indicare di non aver
+      ancora trovato una compatibilità
+*/
+var mailTrovata = false;
 
-  if (mailUtente == lista[i]) {
-    console.log("Accesso consentito");
-  } else {
-    console.log("Accesso negato");
+/* 4. Definisco un ciclo for per controllare la email inserita ed eventualmente
+      modificare il valore booleano della variabile mailTrovata in true
+*/
+for (var i = 0; i < emails.length; i++) {
+
+  if (emailUtente == emails[i]) {
+    mailTrovata = true;
   }
+}
 
+if (mailTrovata == true) {
+  console.log("email corretta");
+} else {
+  console.log("email errata");
 }
